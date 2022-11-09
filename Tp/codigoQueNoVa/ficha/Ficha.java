572446f3e5aace8aa8362f6da503bb3c5504ed26
lara-scazzola1 <83927746+lara-scazzola1@users.noreply.github.com>
2022-11-09 17:14:43 -0300

    Delete Tp directory

--- Tp/codigoQueNoVa/ficha/Ficha.java
+++
@@ -1,6 +0,0 @@
-package ficha;
-
-public interface Ficha {
-	
-	public boolean moverFicha(Ficha ficha);
-}
--- Tp/src/main/java/excepciones/ExcepcionMovimientoInvalido.java
+++
@@ -1,7 +0,0 @@
-package excepciones;
-
-public class ExcepcionMovimientoInvalido extends RuntimeException {
-	public ExcepcionMovimientoInvalido(){
-	       super("Movimiento invalido");
-	   }
-}
