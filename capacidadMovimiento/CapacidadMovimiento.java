3656906c4af8528a2f44de638f00180f25b109e3
lara-scazzola1 <83927746+lara-scazzola1@users.noreply.github.com>
2022-11-13 15:30:47 -0300

    Add files via upload

---
+++ capacidadMovimiento/CapacidadMovimiento.java
@@ -0,0 +1,10 @@
+package capacidadMovimiento;
+
+import tablero. *; 
+
+public interface CapacidadMovimiento {
+	
+	public boolean validarMovimiento(int posXi, int posYi, int posXf, int posYf, Tablero tablero);
+	
+	public int[] movimientoPosible(int posXi, int posYi, Tablero tablero);
+}
