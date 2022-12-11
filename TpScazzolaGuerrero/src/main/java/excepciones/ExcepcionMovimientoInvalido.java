a980e813c5a719eb626bee79d0bd68e90d95c974
marttinguerrero <86444726+marttinguerrero@users.noreply.github.com>
2022-12-11 14:12:05 -0300

    Delete TpScazzolaGuerrero directory

--- TpScazzolaGuerrero/src/main/java/excepciones/ExcepcionMovimientoInvalido.java
+++
@@ -1,9 +0,0 @@
-package excepciones;
-
-public class ExcepcionMovimientoInvalido extends RuntimeException implements java.io.Serializable{
-	private static final long serialVersionUID = 1L;
-	
-	public ExcepcionMovimientoInvalido(String mensaje){
-	       super(mensaje);
-	   }
-}
--- TpScazzolaGuerrero/src/main/java/excepciones/ExcepcionTurnoEquivocado.java
+++
@@ -1,9 +0,0 @@
-package excepciones;
-
-public class ExcepcionTurnoEquivocado extends RuntimeException implements java.io.Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	public ExcepcionTurnoEquivocado(){
-        super("Es turno del equipo contrario");
-    }
-}
